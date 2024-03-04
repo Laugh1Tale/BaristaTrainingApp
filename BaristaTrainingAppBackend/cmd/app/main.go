5d856242c90465475a7dc6c8e951243dd0a6d792
Laugh1Tale <saharus@outlook.com>
2024-03-05 00:50:34 +0500

    add connection to db, configs

--- BaristaTrainingAppBackend/cmd/app/main.go
+++ BaristaTrainingAppBackend/cmd/app/main.go
@@ -6,7 +6,10 @@ import (
 	baristaTrainingApp "barTrApp/pkg/server"
 	"barTrApp/pkg/service"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -15,7 +18,24 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repository := repository.NewRepository()
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db %s", err.Error())
+	}
+
+	repository := repository.NewRepository(db)
 	services := service.NewService(repository)
 	handlers := handler.NewHandler(services)
 
@@ -26,7 +46,7 @@ func main() {
 }
 
 func initConfig() error {
-	viper.AddConfigPath("configs")
+	viper.AddConfigPath("../../configs")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
 }
