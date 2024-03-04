b78807cbbfea4f3f7854b1b4bfc1948092777e80
Laugh1Tale <saharus@outlook.com>
2024-03-04 18:33:15 +0500

    initial commit, add DB scheme

---
+++ BaristaTrainingAppBackend/cmd/app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println("this is with go mod")
+}
+
+//swag for documentaion swagger auto gen
+//swag init -g internal/app/app.go
---
+++ BaristaTrainingAppBackend/go.mod
@@ -0,0 +1,3 @@
+module vscode-tutorial
+
+go 1.22.0
